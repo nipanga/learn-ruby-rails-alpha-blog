0e63b95730eebcc15ef296eaec6b1bb48a867432
Juliano A. Felipe <jotafelipe@gmail.com>
2020-05-21 15:30:22 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# learn/ruby-rails/alpha-blog
+
+"Learning is the only thing the mind never exhausts, never fears, and never regrets." - Da Vinci.
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,86 @@
+class ArticlesController < ApplicationController
+
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @articles = Article.all
+  end
+
+
+  def show
+  end
+
+
+  def new
+    @article = Article.new
+  end
+
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was created successfully.'
+      redirect_to @article
+    else
+      render 'new'
+    end
+    # respond_to do |format|
+    #   if @article.save
+    #     format.html { redirect_to @article, notice: 'Article was successfully created.' }
+    #     format.json { render :show, status: :created, location: @article }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+
+  def edit
+  end
+
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = 'Article was updated successfully.'
+      redirect_to @article
+    else
+      render 'edit'
+    end
+
+    # respond_to do |format|
+    #   if @article.update(article_params)
+    #     format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+    #     format.json { render :show, status: :ok, location: @article }
+    #   else
+    #     format.html { render :edit }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    #   end
+    # end
+  end
+
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+    # respond_to do |format|
+    #   format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+    #   format.json { head :no_content }
+    # end
+  end
+
+
+  private
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+      #params.fetch(:article, {})
+    end
+
+end
